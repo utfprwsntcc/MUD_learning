70dc2df69db41e2000a3f3da044895892bf5ffbc
AokiMarcel <42128895+utfprwsntcc@users.noreply.github.com>
2019-04-12 02:34:44 -0300

    Add files via upload

---
+++ LearningProject/Objects.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LearningProject
+{
+    public class Actor
+    {
+        private int id;
+        private int healthPoints;
+        private int attackPoints;
+        private int armorPoints;
+
+        public Actor(int healthPoints, int attackPoints, int armorPoints)
+        {
+            //auto increment #id
+            this.healthPoints = healthPoints;
+            this.attackPoints = attackPoints;
+            this.armorPoints = armorPoints;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public int HealthPoints { get => healthPoints; set => healthPoints = value; }
+        public int AttackPoints { get => attackPoints; set => attackPoints = value; }
+        public int ArmorPoints { get => armorPoints; set => armorPoints = value; }
+    }
+
+    public class Player : Actor
+    {
+        object[] playerEquips = new object[1];
+
+        public Player(int healthPoints, int attackPoints, int armorPoints) : base(healthPoints, attackPoints, armorPoints)
+        {
+        }
+
+       
+    }
+
+
+}
